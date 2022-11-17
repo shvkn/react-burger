dc6d85d851f7b72364fa297746f07312af788549
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2022-11-17 23:03:06 +0700

    refactor: Рефакторинг функций работы с API
    Signed-off-by: Anatoly Shuvakhin <a.shuvakhin@gmail.com>

--- src/utils/burger-api.js
+++ src/utils/burger-api.js
@@ -5,17 +5,22 @@ const processResponse = (res) => {
   throw new Error(`Ошибка: ${res.status}`);
 };
 
+const request = (url, options) => {
+  return fetch(url, options).then(processResponse);
+};
+
 export const getIngredients = () => {
-  return fetch(`${NORMA_API}/ingredients`).then(processResponse);
+  return request(`${NORMA_API}/ingredients`);
 };
 
 export const postOrder = (ingredients) => {
-  return fetch(`${NORMA_API}/orders`, {
+  const options = {
     method: 'POST',
     headers: {
       authorization: 'a9c10068-1239-4b61-97d8-9278a4fcdf82',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ ingredients }),
-  }).then(processResponse);
+  };
+  return request(`${NORMA_API}/orders`, options);
 };
