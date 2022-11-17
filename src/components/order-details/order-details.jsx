504272ee98bbaf8ad43de79d45f3d4962481a117
Anatoly Shuvakhin <a.shuvakhin@gmail.com>
2022-11-17 15:40:54 +0700

    refactor: Убрать обертки `inner` в компонентах `IngredientDetails`, `OrderDetails`
    Signed-off-by: Anatoly Shuvakhin <a.shuvakhin@gmail.com>

--- src/components/order-details/order-details.jsx
+++ src/components/order-details/order-details.jsx
@@ -1,19 +1,23 @@
 import React from 'react';
 import styles from './order-details.module.css';
 import doneImage from '../../images/done.svg';
+import PropTypes from 'prop-types';
 
-function OrderDetails() {
+function OrderDetails({ number }) {
   return (
-    <div className={`mt-4 mb-30 ${styles.inner}`}>
-      <p className={`text text_type_digits-large ${styles.id}`}>034536</p>
+    <>
+      <p className={`mt-4 text text_type_digits-large ${styles.id}`}>{number}</p>
       <p className={`mt-8 text text_type_main-medium`}>идентификатор заказа</p>
       <img className={`mt-15`} src={doneImage} alt='Заказ оформлен' />
       <p className={`mt-15 text text_type_main-default`}>Ваш заказ начали готовить</p>
-      <p className={`mt-2 text text_type_main-default text_color_inactive`}>
+      <p className={`mt-2 mb-30  text text_type_main-default text_color_inactive`}>
         Дождитесь готовности на орбитальной станции
       </p>
-    </div>
+    </>
   );
 }
 
+OrderDetails.propTypes = {
+  number: PropTypes.number.isRequired,
+};
 export default OrderDetails;
